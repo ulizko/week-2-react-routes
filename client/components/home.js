4f6f946fe49574c365cd2d192a3c10c4e8df0ed2
Oleksandr Ulizko <ulizman@gmail.com>
2020-05-13 10:28:27 +0300

    Fix routes

--- client/components/home.js
+++ client/components/home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Header from './header'
 import Dashboard from './dashboard'
@@ -13,9 +13,9 @@ const Home = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <Switch>
-            <Route exact path="/dashboard/" component={() => <Dashboard />} />
-            <Route exact path="/dashboard/main" component={() => <Main />} />
-            <Route exact path="/dashboard/profile/:username" component={() => <Profile />} />
+            <Route exact path="/" component={() => <Dashboard />} />
+            <Route exact path="/main" component={() => <Main />} />
+            <Route exact path="/profile/:username" component={() => <Profile />} />
           </Switch>
         </div>
       </div>
